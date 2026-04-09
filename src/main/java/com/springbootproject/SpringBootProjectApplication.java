2ebf98c933736b26d2916dc683d3e5e5f9fbd010
Android App <android@app.com>
2026-04-09 22:30:53 +0800

    refactor: 重构数据库配置为H2并优化组件扫描
    将MySQL数据库配置改为H2内存数据库，便于开发和测试
    添加@ComponentScan注解明确扫描包路径
    统一实体类扫描配置，优化JPA相关设置

--- src/main/java/com/springbootproject/SpringBootProjectApplication.java
+++ src/main/java/com/springbootproject/SpringBootProjectApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan("com.springbootproject")
 @EnableJpaRepositories("com.springbootproject.Repository")
-@EntityScan("com.springbootproject.Entity")
+@EntityScan(basePackages = "com.springbootproject")
 @EnableScheduling
 public class SpringBootProjectApplication {
 
